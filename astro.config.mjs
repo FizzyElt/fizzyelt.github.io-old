3b1c7b3a458b5925d3090d52ed0fcb629714b9ea
FizzyElt <fizzyelt8786@gmail.com>
2022-09-05 20:19:57 +0800

    markdown support latex

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
+import remarkMath from 'remark-math';
+import rehypeKaTex from 'rehype-katex';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fizzyelt.github.io',
   integrations: [mdx(), sitemap(), react(), tailwind()],
   markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKaTex],
     shikiConfig: {
       theme: 'one-dark-pro',
     },
---
+++ src/pages/blog/test.md
@@ -0,0 +1,14 @@
+---
+layout: "../../layouts/BlogPost.astro"
+title: "test"
+pubDate: "2022/09/1"
+description: ""
+tags: "Rust,asynchronous"
+---
+
+
+# test
+hello $D$ $F$ 
+$$
+f:X\to Y
+$$
