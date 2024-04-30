0e7dd3e485e1a865558a68bf880695007dc67dff
FizzyElt <fizzyelt.dev@outlook.com>
2024-04-30 22:30:47 +0800

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fizzyelt.github.io',
-  integrations: [sitemap(), tailwind()],
-  markdown: {
-    remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
-    shikiConfig: {
-      theme: 'one-dark-pro',
-    },
-  },
+	site: "https://fizzyelt.com",
+	integrations: [sitemap(), tailwind()],
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+		shikiConfig: {
+			theme: "one-dark-pro",
+		},
+	},
 });
